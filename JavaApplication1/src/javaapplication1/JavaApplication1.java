64cdc01ddd80f707b8a3acd4b628b1ba2ed364ae
hp-hp <hp-hp@hp>
2016-02-18 10:37:37 +0530

    18-02-2016

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hi");
+        
         // TODO code application logic here
     }
     
